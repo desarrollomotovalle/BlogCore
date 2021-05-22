2d5cc3030b329e97cb5e479ce7359b696fda7204
Cesar Augusto Ariza <cariza@MotCom.com>
2021-05-22 18:27:07 -0500

    Organizacion del proyecto por niveles

--- BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
+++ BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BlogCore.Data
+namespace BlogCore.AccesoDatos.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- BlogCore/Startup.cs
+++ BlogCore/Startup.cs
@@ -1,4 +1,4 @@
-using BlogCore.Data;
+using BlogCore.AccesoDatos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
