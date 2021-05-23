3c1371d7e3dca2efd4d18b84287cc25b94cc8f97
Cesar Augusto Ariza <cariza@MotCom.com>
2021-05-23 13:47:57 -0500

    Creacion de controlador articulos, View models y articulo index

---
+++ BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,41 @@
+﻿using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+         public IActionResult Index()
+         {
+            ArticuloVM articulovm = new ArticuloVM()
+            {
+                Articulo = new Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+            };
+
+            return View(articulovm);
+         }
+
+        #region Llamadas a las API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Agregamos las propiedades de categoria porque hay una foreingKey
+            return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties:"Categoria") });
+        }
+
+        #endregion
+    }
+}
