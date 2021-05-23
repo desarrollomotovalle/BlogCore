0313ef2870f4294f4cfe1fda4abd32662e2fc113
Cesar Augusto Ariza <cariza@MotCom.com>
2021-05-22 23:07:21 -0500

    Creacion de repositorio IRepository, ICategoriaRepository y contenedor de trabajo

---
+++ BlogCore.AccesoDatos/Data/Repository/ICategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public interface ICategoriaRepository : IRepository<Categoria>
+    {
+
+        IEnumerable<SelectListItem> GetListaCategorias();
+
+        void Update(Categoria categoria);
+
+    }
+}
---
+++ BlogCore.Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [Display(Name = "Nombre categoría")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Orden de visualización")]
+        public string Orden { get; set; }
+
+
+    }
+}
